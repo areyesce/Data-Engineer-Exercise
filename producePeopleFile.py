c7aab0033625dad6e064573eaa4b17b111cec7a6
AVR <areyesc1@swarthmore.edu>
2023-03-03 22:32:24 -0700

    Update producePeopleFile.py

--- producePeopleFile.py
+++ producePeopleFile.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+# making data frame 
+data = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv") 
+    
+# calling head() method  
+# storing in new variable 
+data_top = data.head() 
+    
+print(data_top)
+# display 
+data_top 
