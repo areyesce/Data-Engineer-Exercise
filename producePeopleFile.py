c82d7ffe73796f576a8ebd282ea31d04684df4e5
AVR <areyesc1@swarthmore.edu>
2023-03-04 16:45:05 -0700

    Update producePeopleFile.py

--- producePeopleFile.py
+++ producePeopleFile.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # making data frame 
-# const_info = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv") 
-# const_emails = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv") 
+const_info = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv") 
+const_emails = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv") 
 const_sub_status = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv")
 
 # const_info_top = const_info.head() 
@@ -23,14 +23,58 @@ const_sub_status = pd.read_csv("https://als-hiring.s3.amazonaws.com/fake_data/20
 """
 Produce a “people” file with the following schema. 
 TODO: Save it as a CSV with a header line to the working directory.
+* We only care about subscription statuses where chapter_id is 1. *
 """
 # total number of entries where chapter_id is 1, 275484/350000
 # print(len(const_sub_status[const_sub_status['chapter_id']==1]))
 
+"""df with entries where chapter_id is 1"""
 chapter_id_1_rows = const_sub_status.loc[const_sub_status['chapter_id'] == 1]
 """TODO: may need to reset index"""
-print(chapter_id_1_rows.info())
-print(chapter_id_1_rows.head(10))
+# print(chapter_id_1_rows.info())
+# print(chapter_id_1_rows.head(10))
+
+"""TODO: ADD HEADER TO CSV CREATED"""
+
+""" save list of cons_email_id values """
+cons_email_id_list = chapter_id_1_rows['cons_email_id'].to_list()
+# print("cons email list") 
+# print(cons_email_id_list)
+# print(chapter_id_1_rows['cons_email_id'].value_counts()) # unique
+
+""" get rows from const emails df that are in cons_email_id_list and are primary email """
+# 173990/275484 were is_primary
+const_emails_primary = const_emails[(const_emails.cons_email_id.isin(cons_email_id_list)) & (const_emails.is_primary == 1)]
+
+# print(const_emails_primary.info())
+# print(const_emails_primary.head(10))
+
+""" from here could choose to just drop columns and then merge from third file """
+""" sort primary list by cons_email_id """
+const_emails_primary_sorted = const_emails_primary.sort_values(by='cons_email_id')
+print(const_emails_primary_sorted.info())
+print(const_emails_primary_sorted.head(10))
+
+""" save list of cons_email_id values """
+cons_email_id_list_from_primary = const_emails_primary_sorted['cons_email_id'].to_list()
+# print(cons_email_id_list_from_primary)
+
+isunsub_df = const_sub_status[const_sub_status.cons_email_id.isin(cons_email_id_list_from_primary)]
+# print(isunsub_df.info())
+# print(isunsub_df.head(10))
+
+"""
+NOTE: df sizes # entries do not match, may need to delete duplicates
+"""
+isunsub_df_sorted = isunsub_df.sort_values(by='cons_email_id')
+isunsub_df_sorted.drop_duplicates(subset='cons_email_id',keep=False, inplace=True)
+print(isunsub_df_sorted.info()) # 134259 entries
+print(isunsub_df_sorted.head(10))
+
+
+""" sort by cons_email_id before joining"""
+
+
 
 
 
