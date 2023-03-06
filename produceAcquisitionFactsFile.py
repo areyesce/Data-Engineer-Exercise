008568a4062d6c9f5da00f1e98c318c8ffac90af
AVR <areyesc1@swarthmore.edu>
2023-03-05 21:06:17 -0700

    Update produceAcquisitionFactsFile.py

--- produceAcquisitionFactsFile.py
+++ produceAcquisitionFactsFile.py
@@ -2,17 +2,11 @@ import pandas as pd
 
 people_info = pd.read_csv("people.csv")
 
-# print(people_info.info())
-# print(people_info.head(10))
-
+"""" shorten the timestamp to only include the date field of the object """
 people_info['created_dt'] = pd.to_datetime(people_info['created_dt']).dt.date
-# print(people_info.head(10))
 
 """ sorted for easy plotting """
 val_counts_df = people_info['created_dt'].value_counts().rename_axis('acquisition_date').reset_index(name='acquisitions').sort_values(by='acquisition_date').reset_index(drop=True)
-
-# print(val_counts_df.info())
-# print(val_counts_df)
 
 val_counts_df.to_csv("acquisition_facts.csv", index=False)
 
